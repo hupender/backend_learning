5ca07f5c6b9ae8113f7bee2bfde67ff5ef36b11f
hupender <hupenderkhatod>
2023-09-30 00:02:19 +0530

    add delete option

--- To-Do-List/index.js
+++ To-Do-List/index.js
@@ -30,32 +30,6 @@ app.post("/work",(req,res)=> {
     })
 });
 
-
-// app.post("/",(req,res)=> {
-//     var task_type=req.body.to_do;
-//     var task_val=req.body.task;
-//     if(task_type=="Work_related") {
-//         if(task_val) {
-//             item_work.push(task_val);
-//         }
-//         // res.render("index.ejs",{
-//         //     list_work: item_work,
-//         // });
-//     }
-//     else {
-//         if(task_val) {
-//             item_day.push(task_val);
-//         }
-//         // res.render("index.ejs",{
-//         //     list_day: item_day,
-//         // });
-//     }
-//     res.render("index.ejs",{
-//         list_work: item_work,
-//         list_day: item_day,
-//     });
-// });
-
 app.listen(3000,()=> {
     console.log("Server started on port 3000");
 });
--- To-Do-List/public/scripts/script.js
+++ To-Do-List/public/scripts/script.js
@@ -1,13 +1,35 @@
-var target_btn=document.querySelector("#work_list");
-const collection=target_btn.children;
+// code to delete a list element 
+var btn_work=document.querySelectorAll("#work_id button");
+var work_list=document.querySelectorAll("#work_id .left");
 
-// console.log(collection.children.children.length);
-// console.log(collection.length);
-
-let text = "";
-for (let i = 0; i < collection.length; i++) {
-  // text += collection[i].tagName + "<br>";
-  text+=collection[i].getAttribute("class");
-  console.log(text);
+for(var i=0;i<btn_work.length;i++) {
+  btn_work[i].setAttribute("id",i);
 }
-// console.log(text);
+
+for(var i=0;i<btn_work.length;i++) {
+  console.log(btn_work[i]);
+  btn_work[i].onclick=function(e) {
+    work_list[e.target.id].remove();
+    btn_work[e.target.id].remove();
+    console.log(e.target.id);
+    // console.log(btn_work[e.target.id]);
+  }
+}
+
+var btn_day=document.querySelectorAll("#day_id button");
+var day_list=document.querySelectorAll("#day_id .left");
+
+for(var i=0;i<btn_day.length;i++) {
+  btn_day[i].setAttribute("id",i);
+}
+
+for(var i=0;i<btn_day.length;i++) {
+  console.log(btn_day[i]);
+  btn_day[i].onclick=function(e) {
+    day_list[e.target.id].remove();
+    btn_day[e.target.id].remove();
+    console.log(e.target.id);
+  }
+}
+
+
