66a14fbbf673d032ae19a6b84014e696870a73d4
hupender <hupenderkhatod>
2023-10-05 18:42:11 +0530

    json files added

--- To-do-list1/index.js
+++ To-do-list1/index.js
@@ -2,21 +2,51 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 
+mongoose.connect("mongodb://localhost:27017/to_do",{useNewUrlParser: true});
+const toDoSchema = new mongoose.Schema({
+    task: String
+});
+const task=mongoose.model("task",toDoSchema);
+
+
 const app=express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 const port=3000;
-var list=[];
+
 app.get("/",(req,res)=> {
     res.render("index.ejs");
 });
 
 app.post("/",(req,res)=> {
     if(req.body.task) {
-        list.push(req.body.task);
+        // create a new task to push to the db
+        const task1=new task({
+            task: req.body.task
+        });
+        // insert the from data to the db
+        task1.save()
+        .then((result)=> {
+            console.log(result);
+        })
+        .catch((error)=> {
+            console.log(error);
+        });
     }
-    res.render("index.ejs",{
-        list: list,
+    // gets all the elements from the database
+    task.find()
+    .then((results)=> {
+        var task_list=[];
+        for(var i=0;i<results.length;i++) {
+            task_list.push(results[i].task);
+            // console.log(results[i].task);
+        }
+        res.render("index.ejs",{
+            list: task_list,
+        });
+    })
+    .catch((error)=> {
+        console.log(error);
     });
 });
 
--- To-do-list1/public/script/index.js
+++ To-do-list1/public/script/index.js
@@ -1,5 +1,5 @@
 var btn=document.querySelector("button");
 btn.onclick=function(event) {
     console.log(event);
-    event.preventDefault();
+    // event.preventDefault();
 }
